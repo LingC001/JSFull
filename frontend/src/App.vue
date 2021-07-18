a2d9eb44325f03588ba74e02f0058fb306c507eb
杨博 <1070939301@qq.com>
2021-07-18 23:01:23 +0800

    no commit message

---
+++ frontend/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+	<van-cell-group>
+		<van-field v-model.number="total" label="总额" placeholder="请输入当前总额" />
+		<van-field v-model.number="balance" label="余额" placeholder="" />
+	</van-cell-group>
+	<van-row type="flex" justify="center" style="margin-top: 50px;">
+		<van-button round size="normal" type="primary" @click="calculate">开始计算</van-button>
+	</van-row>
+	
+  </div>
+</template>
+
+<script>
+import dayjs from 'dayjs'
+const monFoodCost = 1200
+
+export default {
+  name: 'App',
+  data(){
+	return {
+		total:'',
+		balance:''
+	}
+  },
+  computed:{},
+  methods:{
+	calculate(){
+		let curDay = dayjs().date()
+		let curMonDays = dayjs().daysInMonth()
+		let restDay = curMonDays - curDay + 12
+		let restFoodCost = Math.round(restDay * monFoodCost/curMonDays)
+		this.balance = this.total - restFoodCost
+		console.log("curDay: ",curDay);
+		console.log("curMonDays: ",curMonDays);
+		console.log("restDay: ",restDay);
+	}
+  }
+}
+</script>
+
+<style>
+#app {
+  
+}
+</style>
---
+++ frontend/vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	lintOnSave:'warning',
+	devServer:{
+		public:'192.168.0.157'
+	}
+}
